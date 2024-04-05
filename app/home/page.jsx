b09879752ca6200912fbca48c442b505df475132
vgokani <85189841+v1dh1@users.noreply.github.com>
2024-04-05 12:35:19 -0400

    changed home page

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import Homepage from './Homepage';
+import Projects from '../components/Projects';
 
 const Home = () => {
   return (
-    <div><Homepage/></div>
+    <><div><Homepage /></div><div><Projects /></div></>
   )
 }
 
