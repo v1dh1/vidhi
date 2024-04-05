553ad4a12d0018d2b0d314d1653e7f50648aad3d
vgokani <85189841+v1dh1@users.noreply.github.com>
2024-04-05 10:37:43 -0400

    retry

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,6 @@
-import Image from "next/image";
-import Nav from './components/Nav';
-import Welcome from './components/Welcome';
 import Projects from './components/Projects';
-import Footer from './components/Footer';
 import About from './components/About';
-import NewNav from './components/NewNav';
-import AboutMe from "./about/AboutMe";
+
 export default function Home() {
   return (
     <>
