ba93b381394383d6fc535ad85d57c245de3b5caa
vgokani <85189841+v1dh1@users.noreply.github.com>
2024-04-24 16:38:35 -0400

    so many changes

--- app/about/AboutMe.jsx
+++ app/about/AboutMe.jsx
@@ -23,26 +23,26 @@ const AboutMe = () => {
             />
             {/* You can still place other content here, which will be positioned above the image due to the relative positioning of the container */}
             </div>
-        <div class="text-4xl p-4 text-stone-200">{"Hi, I'm Vidhi"}</div>
+        <div class="text-4xl p-4 text-stone-200 font-sofia">{"Hi, I'm Vidhi"}</div>
     </div>
 
     <div className='pt-8 pb-6 text-2xl text-stone-200 flex justify-center'>{"My Design Philosophy"}</div>
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-        <div class="bg-stone-500 rounded-lg drop-shadow-lg bg-clip-padding backdrop-filter backdrop-blur-l bg-opacity-20 border border-stone-300 flex justify-center flex-col p-4">
+        <div class="bg-stone-500 rounded-lg drop-shadow-lg bg-clip-padding backdrop-filter backdrop-blur-l bg-opacity-20 border-2 border-stone-600 flex justify-center flex-col p-4">
             
             <div className='text-stone-200 text-xl flex justify-center pb-4'>{"Accessibility"}</div>
             <div className='text-stone-400 text-l flex justify-center'>{"I believe design is for everyone. It's about breaking down barriers and creating experiences that are open and usable for people with all abilities. My approach prioritizes accessibility from the ground up, ensuring that each product I design can be easily navigated and enjoyed by as wide an audience as possible."}</div>
             
         </div>
 
-        <div class="bg-stone-500 rounded-lg drop-shadow-lg bg-clip-padding backdrop-filter backdrop-blur-l bg-opacity-20 border border-stone-300 flex justify-center flex-col p-4">
+        <div class="bg-stone-500 rounded-lg drop-shadow-lg bg-clip-padding backdrop-filter backdrop-blur-l bg-opacity-20 border-2 border-stone-600 flex justify-center flex-col p-4">
             
             <div className='text-stone-200 text-xl flex justify-center pb-4'>{"Problem Passionate"}</div>
             <div className='text-stone-400 text-l flex justify-center'>{"My drive comes from tackling challenges head-on. I'm passionate about diving deep into problems, understanding their roots, and crafting innovative solutions. The thrill of turning complex issues into seamless experiences fuels my design process, pushing me to think creatively and empathetically."}</div>
             
         </div>
 
-        <div class="bg-stone-500 rounded-lg drop-shadow-lg bg-clip-padding backdrop-filter backdrop-blur-l bg-opacity-20 border border-stone-300 flex justify-center flex-col p-4">
+        <div class="bg-stone-500 rounded-lg drop-shadow-lg bg-clip-padding backdrop-filter backdrop-blur-l bg-opacity-20 border-2 border-stone-600 flex justify-center flex-col p-4">
             
             <div className='text-stone-200 text-xl flex justify-center pb-4'>{"Simplicity"}</div>
             <div className='text-stone-400 text-l flex justify-center'>{"My design philosophy revolves around stripping away the unnecessary, focusing on what truly matters to create intuitive and meaningful products. By championing simplicity, I aim to deliver designs that are not only elegant but also remarkably user-friendly, making every interaction straightforward and enjoyable."}</div>
@@ -56,7 +56,15 @@ const AboutMe = () => {
     </div>
 
     <div className='grid grid-cols-1 md:grid-cols-4 gap-4 pt-8'>
-        <div className=''></div>
+        <div className='bg-stone-800 border-2 border-stone-600 rounded-xl p-4'>
+            <div className='text-stone-200'>Likes</div>
+            <div className='text-stone-300 pt-2'>{"The color green, avocados, trinkets, "}</div>
+        </div>
+
+        <div className='bg-stone-800 border-2 border-stone-600 rounded-xl p-4'>
+            <div className='text-stone-200'>Dislikes</div>
+            <div className='text-stone-300 pt-2'>{"Onions, "}</div>
+        </div>
         <div className=''></div>
         <div className=''></div>
         <div className=''></div>
--- app/personal-project/Design.jsx
+++ app/personal-project/Design.jsx
@@ -111,9 +111,78 @@ const Design = () => {
         <div className='text-stone-200 pt-4 pb-4'>{"At this stage, I wanted to make sure my designs were intuitive for my prospective users. I conducted usability testing through the pen and paper method, where I asked 2 people to go through a printed version of my low fidelity screens, and gauged their experiences with the app's anticipated interactions. "}</div>
         
         <div className='bg-stone-800 rounded-xl'>
-            <div className='text-stone-200 p-4'>Findings</div>
+            <div className='text-stone-200 p-4 text-xl'>Findings</div>
+            <div className='text-stone-300 px-4'>{"Based on the usability testing, the app was simple enough to navigate. There were a couple points that came up on improving the UI aspect of the app. "}</div>
+
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+
+                <div className='p-4 text-stone-300'>
+                    <div className='font-bold'>{"Product Cards have too much information"}</div>
+                    <div className='pt-2'>{"Too much information at once can be distracting for the user, and lead to them not using the app to the fullest. I realized I needed to continue exploring options for the product cards to pick something that gets the point across."}</div>
+                </div>
+
+                <div className='p-4 text-stone-300'>
+                <div className='font-bold'>{"Essentials buttons at the top can be smaller"}                </div>
+
+                    <div className='pt-2'>{"The users did not see a need for these buttons, especially if there are similar buttons in the bottom navigation. I decided I can try redesigning them, or take them out of the app. "}</div>
+                </div>
+
+            </div>
         </div>
-        
+
+        <div className='text-stone-300 text-center pt-4 pb-4'>{"Following these findings, I continued to make a first iteration of designs for the app."}</div>
+        <div className='flex justify-center'>
+        <Image
+              src="/exploration3.png"
+              width={1000}
+              height={500}
+              alt="iphone mockup of front page"
+            />
+
+
+        </div>
+
+        <div className='text-stone-200 pt-4 text-center'>{"I was still not satisfied with these designs. I made a second iteration instead."}</div>
+        <div className='flex justify-center pt-4 pb-4'>
+        <Image
+              src="/exploration4.png"
+              width={1000}
+              height={500}
+              alt="iphone mockup of front page"
+            />
+
+
+        </div>
+
+        <div className='text-stone-200 pt-4 text-center'>{"Finally, I made my final iteration of the app design. Let's take a look at some components in detail."}</div>
+        <div className='flex justify-center pt-4 pb-4'>
+        <Image
+              src="/finaldesign.png"
+              width={1000}
+              height={500}
+              alt="final design screens"
+            />
+
+
+        </div>
+
+        <div className='grid grid-cols-1 md:grid-cols-2'>
+            <div className='flex justify-center pt-4 pb-4'>
+
+            <Image
+              src="/beads.png"
+              width={500}
+              height={500}
+              alt="zoomed in product card"
+            />
+
+            </div>
+
+            <div className='flex justify-center pt-4 pb-4'>
+            <div className='pl-4 text-stone-200'>{"This product card was designed to be simplistic, and only show the required information needed for users to understand what the product is, and 2 actions they can do with it. The first action would be to"} "like" {"the product (which would add it to their wishlist), and the second would be to click on this card itself, which would lead to the product's page containing more details about the item, and give users the option to contact the lister to barter for the item."}</div>
+
+            </div>
+        </div>
     </div>
 
 </div>
