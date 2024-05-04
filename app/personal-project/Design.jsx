cf02394de58b4d0668ad1544e434f62f9e68a714
vgokani <85189841+v1dh1@users.noreply.github.com>
2024-05-03 23:34:24 -0400

    change

--- app/personal-project/Design.jsx
+++ app/personal-project/Design.jsx
@@ -7,8 +7,8 @@ const Design = () => {
 
 <div className='container mx-auto w-3/4'>
 
-        <div className='text-3xl font-serifDisplay text-stone-100 pt-32'>Design</div>
-        <div className='text-l text-stone-200'>Rationale behind the designs</div>
+        <div className='text-3xl font-serifDisplay text-stone-100 pt-32'>{"Design"}</div>
+        <div className='text-l text-stone-200'>{"Rationale behind the designs"}</div>
 
         <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
         
@@ -27,7 +27,7 @@ const Design = () => {
         </div>
         </div>
 
-        <div className='text-stone-100 text-xl pt-8 pb-4'>Onboarding</div>
+        <div className='text-stone-100 text-xl pt-8 pb-4'>{"Onboarding"}</div>
         <div className='text-stone-200'>{"I stuck to a 3 screen process for onboarding. The onboarding process of any app can make or break whether users will actually use the app or not. Users have limited time which they are ready to devote to learning how the product works before starting to actually interact with it, and I wanted to keep the onboarding short and simple. It should concisely let users know who should use this app, how it will be helpful, and how to use it. "}</div>
 
         <div className='grid grid-cols-1 md:grid-cols-2'>
@@ -46,9 +46,9 @@ const Design = () => {
             <div className=''>hiii</div>
         </div>
 
-        <div className='text-stone-200 text-center pt-2'>The before and the after of onboarding</div>
+        <div className='text-stone-200 text-center pt-2'>{"The before and the after of onboarding"}</div>
 
-        <div className='text-stone-100'>This project was a really iterative process. I started with barebone wireframes of what the main screen of this app may look like</div>
+        <div className='text-stone-100'>{"This project was a really iterative process. I started with barebone wireframes of what the main screen of this app may look like"}</div>
 
         <div className='grid grid-cols-1 md:grid-cols-2'>
        
@@ -111,7 +111,7 @@ const Design = () => {
         <div className='text-stone-200 pt-4 pb-4'>{"At this stage, I wanted to make sure my designs were intuitive for my prospective users. I conducted usability testing through the pen and paper method, where I asked 2 people to go through a printed version of my low fidelity screens, and gauged their experiences with the app's anticipated interactions. "}</div>
         
         <div className='bg-stone-800 rounded-xl'>
-            <div className='text-stone-200 p-4 text-xl'>Findings</div>
+            <div className='text-stone-200 p-4 text-xl'>{"Findings"}</div>
             <div className='text-stone-300 px-4'>{"Based on the usability testing, the app was simple enough to navigate. There were a couple points that came up on improving the UI aspect of the app. "}</div>
 
             <div className='grid grid-cols-1 md:grid-cols-2'>
