626ada71c2c22a8ecbd1d59baead105f3e9e4a92
vgokani <85189841+v1dh1@users.noreply.github.com>
2025-01-27 17:51:10 -0500

    changed page layout for wp

--- app/design/wealthyplanet/page.jsx
+++ app/design/wealthyplanet/page.jsx
@@ -32,25 +32,23 @@ const WealthyPlanet = () => {
   
   
         {/* Small screens - LefthomeSmall as a top navbar */}
-        <div className="md:hidden">
+        <div className="">
           <LefthomeSmall />
           <main className="bg-white overflow-y-auto"> {/*removed p-4*/}
-            <Wpproject />
+            <div className=' md:p-12'><Wpproject /></div>
           </main>
         </div>
   
-        {/* Medium and large screens - Lefthome and Projects side by side */}
+        {/* Medium and large screens - Lefthome and Projects side by side 
         <div className="hidden md:grid grid-cols-1 md:grid-cols-4">
-          {/* Lefthome takes up 1/4 of the screen */}
           <aside className="bg-pink-50 md:col-span-1 sticky top-0 h-screen p-4 overflow-y-auto">
             <Lefthome />
           </aside>
-  
-          {/* Projects takes up 3/4 of the screen */}
-          <main className="bg-white md:col-span-3 overflow-y-auto">
+            <main className="bg-white md:col-span-3 overflow-y-auto">
             <Wpproject />
           </main>
         </div>
+        */}
         </motion.div>
   
       </div>
