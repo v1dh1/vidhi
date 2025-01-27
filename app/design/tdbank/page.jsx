4c9f22aec386356fddcd3f18e346a074f3978254
vgokani <85189841+v1dh1@users.noreply.github.com>
2025-01-27 18:11:17 -0500

    getting rid of lefthome for trace

--- app/design/tdbank/page.jsx
+++ app/design/tdbank/page.jsx
@@ -29,25 +29,22 @@ const Tdbank = () => {
 
 
       {/* Small screens - LefthomeSmall as a top navbar */}
-      <div className="md:hidden">
+      <div className="">
         <LefthomeSmall />
-        <main className="bg-white p-4 overflow-y-auto">
+        <main className="bg-white pt-16 p-4 md:p-16 overflow-y-auto">
           <TraceProject />
         </main>
       </div>
 
-      {/* Medium and large screens - Lefthome and Projects side by side */}
+      {/* Medium and large screens - Lefthome and Projects side by side 
       <div className="hidden md:grid grid-cols-1 md:grid-cols-4">
-        {/* Lefthome takes up 1/4 of the screen */}
         <aside className="bg-pink-50 md:col-span-1 sticky top-0 h-screen p-4 overflow-y-auto">
           <Lefthome />
         </aside>
-
-        {/* Projects takes up 3/4 of the screen */}
         <main className="bg-white md:col-span-3 p-4 overflow-y-auto">
           <TraceProject />
         </main>
-      </div>
+      </div>*/}
       </motion.div>
 
     </div>
